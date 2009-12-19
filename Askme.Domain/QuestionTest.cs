74fc886c95c47068ed3b98e1093f84436984fb68
unknown <shanum@.corporate.thoughtworks.com>
2009-12-19 15:46:11 +0530

    Question Class -Shanu/Sri

---
+++ Askme.Domain/QuestionTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Askme.Domain
+{
+    [TestFixture]
+    public class QuestionTest
+    {
+        [Test]
+        public void ShouldBeAbleToGetTheQuestionText()
+        {
+            string questionText = "What is the use of 'var' key word?";
+            Question question = new Question(questionText);
+            Assert.AreEqual(questionText,question.Text);
+        }
+    }
+}
