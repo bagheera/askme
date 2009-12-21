152b3ed7e968c9481cfad36a8d85e0d6b82f87c6
unknown <chandrav@.corporate.thoughtworks.com>
2009-12-21 16:54:30 +0530

    Added user to answer - chandra and gogo

--- Askme.Domain/Answer.cs
+++ Askme.Domain/Answer.cs
@@ -5,10 +5,17 @@ namespace Askme.Domain
     public class Answer
     {
         private readonly AskMeDate createdOn;
+        private readonly User user;
 
-        public Answer(AskMeDate createdOn)
+        public Answer(AskMeDate createdOn, User user)
         {
             this.createdOn = createdOn;
+            this.user = user;
+        }
+
+        public User User
+        {
+            get { return user; }
         }
 
         public AskMeDate CreatedOn
--- Askme.Domain/AnswerTest.cs
+++ Askme.Domain/AnswerTest.cs
@@ -9,8 +9,17 @@ namespace Askme.Domain
         public void AnswerKnowsWhenItWasCreated()
         {
             AskMeDate time = AskMeDate.DefaultTime;
-            Answer answer = new Answer(time);
+            Answer answer = new Answer(time, null);
             Assert.AreEqual(time, answer.CreatedOn);
+        }
+
+        [Test]
+        public void AnswerKnowsTheUserWhoAnswered()
+        {
+            const string userName = "PakodaSingh";
+            User user = new User(userName, "******", "diptanu@thoughtworks.com");
+            Answer answer = new Answer(AskMeDate.DefaultTime,user);
+            Assert.AreEqual(user, answer.User);
         }
     }
 }
